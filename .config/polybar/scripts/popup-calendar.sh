0d2f9de030af88a7a8f414315c76801ee54f0f5d
namnh <namnh.ftu@gmail.com>
2022-01-11 16:29:27 +0700

    update polybar icons

--- .config/polybar/scripts/popup-calendar.sh
+++ .config/polybar/scripts/popup-calendar.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-BAR_HEIGHT=12  # polybar height
+BAR_HEIGHT=22  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +"%a %d %H:%M")"
+DATE="$(date +"  %a,%b %d 󰥔 %H:%M")"
 
 case "$1" in
 --popup)
